536f0aceada83a34922903812b4a43a6c9f47cfa
Q-T5 <bikathimartin@outlook.com>
2023-06-30 07:57:54 +0300

    Add app footer component

---
+++ components/DefaultFooter.vue
@@ -0,0 +1,19 @@
+<template>
+	<footer class="border-t-2 text-center space-y-1 font-nunito">
+		<h1>Open Source 2023 - Apache 2.0 License</h1>
+		<h2>Created By 
+            <span class="transition-all duration-500 hover:text-purple-600 hover:cursor-pointer">Bikathi Martin</span>
+        </h2>
+		<div class="flex place-content-center">
+			<a
+				href="https://github.com/bikathi/cinemax"
+				target="_blank"
+                class="transition hover:-translate-y-2 hover:scale-110 duration-500">
+				<img
+					src="/core-svg/github-icon.svg"
+					alt="gh"
+					class="w-10" />
+			</a>
+		</div>
+	</footer>
+</template>
