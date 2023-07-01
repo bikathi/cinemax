347c454db3591d365262bcdca5e4c3e9ffc67cfb
Q-T5 <bikathimartin@outlook.com>
2023-07-01 23:02:23 +0300

    Improve UI of header and footer

--- components/DefaultFooter.vue
+++ components/DefaultFooter.vue
@@ -1,14 +1,18 @@
 <template>
-	<footer class="border-t-2 text-center space-y-1 font-nunito">
+	<footer class="border-t-[1px] text-center font-nunito">
 		<h1>Open Source 2023 - Apache 2.0 License</h1>
-		<h2>Created By 
-            <span class="transition-all duration-500 hover:text-purple-600 hover:cursor-pointer">Bikathi Martin</span>
-        </h2>
+		<h2>
+			Created By
+			<span
+				class="transition-all duration-500 hover:text-purple-600 hover:cursor-pointer"
+				>Bikathi Martin</span
+			>
+		</h2>
 		<div class="flex place-content-center">
 			<a
 				href="https://github.com/bikathi/cinemax"
 				target="_blank"
-                class="transition hover:-translate-y-2 hover:scale-110 duration-500">
+				class="transition hover:-translate-y-2 hover:scale-110 duration-500">
 				<img
 					src="/core-svg/github-icon.svg"
 					alt="gh"
--- components/DefaultNav.vue
+++ components/DefaultNav.vue
@@ -1,6 +1,32 @@
+<script setup lang="js">
+	const routeLinks = [
+		{name: "Home", toName: "home"},
+		{name: "Movies", toName: "movies"},
+		{name: "About", toName: "about"},
+	];
+
+	const route = useRoute();
+</script>
+
 <template>
-	<nav class="py-2 text-center border-b-2">
-		<NuxtLink to="/" class="font-ubuntu text-2xl">Cinemax</NuxtLink>
-		<h2 class="font-nunito">Easily Book Movie Tickets Online</h2>
+	<nav class="py-2 text-center border-b-2 flex items-center">
+		<div class="w-1/2 text-start ml-5">
+			<NuxtLink
+				to="/"
+				class="font-ubuntu text-2xl"
+				>Cinemax</NuxtLink
+			>
+			<h2 class="font-nunito">Easily Book Movie Tickets Online</h2>
+		</div>
+		<div class="w-1/2 text-start px-10">
+			<NuxtLink
+				v-for="(link, index) in routeLinks"
+				:key="index"
+				:to="{ name: link.toName }"
+				:class="route.name === link.toName ? 'active-route-link' : ''"
+				class="font-roboto text-lg mx-2 tracking-wide hover:transition-all hover:border-b-2 hover:border-b-purple-600 duration-300"
+				>{{ link.name }}</NuxtLink
+			>
+		</div>
 	</nav>
 </template>
