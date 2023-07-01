b6322e2d1bf8e73ba646580e919a4307417dc803
Q-T5 <bikathimartin@outlook.com>
2023-07-01 22:11:31 +0300

    Delete unused app components

--- app.vue
+++
@@ -1,7 +0,0 @@
-<template>
-	<div class="dark:bg-gradient-to-tr dark:text-white from-slate-700 via-slate-900 to-black">
-		<NuxtLayout>
-			<NuxtPage />
-		</NuxtLayout>
-	</div>
-</template>
--- layouts/custom.vue
+++
@@ -1,7 +0,0 @@
-<template>
-	<div>
-		<h1>custom layout</h1>
-		<h2>line just to prove this is a custom layout</h2>
-		<slot />
-	</div>
-</template>
