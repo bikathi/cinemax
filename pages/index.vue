443163724ee48c5187cf0ccb28ca061b30d9ef7b
Q-T5 <bikathimartin@outlook.com>
2023-07-01 22:17:20 +0300

    Give the page a name

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,19 @@
+<script setup lang="js">
+	definePageMeta({
+		name: "home"
+	})
+</script>
+
 <template>
-	<div class="h-screen flex  place-content-center place-items-center">
+	<div class="h-screen flex place-content-center place-items-center">
 		<div class="w-1/2 flex flex-col justify-center space-y-2">
-			<h1 class="font-roboto text-6xl font-normal text-center">Cinemax</h1>
+			<h1 class="font-roboto text-6xl font-normal text-center">
+				Cinemax
+			</h1>
 			<h2 class="font-normal text-3xl">
 				Tired of Waiting In-Line to Get a Cinema Ticket?
 			</h2>
-			
+
 			<div class="place-items-center p-1 flex justify-center">
 				<input
 					type="text"
