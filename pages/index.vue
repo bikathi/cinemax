73e9888dea5ba33844579dde1cc7628cd598d4b1
Q-T5 <bikathimartin@outlook.com>
2023-07-01 22:01:10 +0300

    Create basic layout of homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,11 @@
 <template>
-	<div class="h-screen flex px-2">
+	<div class="h-screen flex  place-content-center place-items-center">
 		<div class="w-1/2 flex flex-col justify-center space-y-2">
-			<h1 class="font-roboto text-6xl font-normal">Cinemax</h1>
+			<h1 class="font-roboto text-6xl font-normal text-center">Cinemax</h1>
 			<h2 class="font-normal text-3xl">
 				Tired of Waiting In-Line to Get a Cinema Ticket?
 			</h2>
-			<p class="font-nunito text-xl">
-				Well then, you're in luck. At Cinemax, we pride ourselves
-				in ensuring you no longer have to queue in lines to enjoy
-				movie nights. Book your tickets for any one of our cinemas for
-				the movie of your choice and conviniently pay with your M-Pesa.
-			</p>
+			
 			<div class="place-items-center p-1 flex justify-center">
 				<input
 					type="text"
@@ -22,12 +17,6 @@
 					>Search</NuxtLink
 				>
 			</div>
-		</div>
-		<div class="w-1/2 flex flex-col justify-center items-center">
-			<img
-				src="/images/clear-bg-landing.png"
-				alt="landing-img"
-				class="w-2/3" />
 		</div>
 	</div>
 </template>
