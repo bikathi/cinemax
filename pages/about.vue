e85498cc2f6e1fd54096d1d054858f2fb71ba8ea
Q-T5 <bikathimartin@outlook.com>
2023-07-02 14:39:03 +0300

    Add about statement

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,22 @@
 <script setup lang="js">
+	import { marked } from "marked";
+	import privacy from "../PRIVACY.md?raw";
+
 	definePageMeta({
-	    name: "about"
+		name: "about"
 	})
 </script>
 
 <template>
-	<h1>welcome to about section</h1>
+	<main class="p-4 flex-grow flex justify-center">
+		<div
+			class="[&>h2]:font-roboto [&>h2]:text-xl [&>h2]:tracking-wide [&>h2]:my-2 [&>p]:font-nunito [&>h3]:font-roboto [&>h3]:my-2 [&>blockquote>p]:font-roboto [&>blockquote>p]:tracking-wide md:max-w-[65%] [&>h1]:font-roboto [&>h1]:text-2xl [&>p]:md:text-lg [&>h3]:tracking-wide"
+			v-html="
+				marked.parse(privacy, {
+					gfm: true,
+					mangle: false,
+					headerIds: false,
+				})
+			"></div>
+	</main>
 </template>
