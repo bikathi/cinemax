5f9986a09454e8c0c4f7e5f2780579ada96eaaf2
Q-T5 <bikathimartin@outlook.com>
2023-07-04 17:03:30 +0300

    Create component to display the movies

---
+++ components/MovieDisplay.vue
@@ -0,0 +1,39 @@
+<script setup lang="js">
+	const componentProps = defineProps({
+	   posterLink: {
+	       type: String,
+	       default: null,
+	       required: true
+	   },
+	   movieId: {
+	       default: null,
+	       required: true
+	   },
+	   movieName: {
+	       type: String,
+	       default: null,
+	       required: true
+	   }
+	});
+</script>
+
+<template>
+	<div class="flex flex-col h-fit relative group">
+		<img
+			:src="posterLink"
+			alt="movie-poster"
+			class="h-[95%] w-full rounded-md object-fill group-hover:brightness-75" />
+		<h1 class="font-nunito truncate">{{ movieName }}</h1>
+		<div
+			class="absolute top-[43%] w-full h-fit hidden group-hover:flex items-center justify-center">
+			<NuxtLink
+				:to="{
+					name: 'details',
+					query: { name: movieName.trim(), id: movieId },
+				}"
+				class="nuxt-link w-fit mt-0"
+				>Learn More</NuxtLink
+			>
+		</div>
+	</div>
+</template>
