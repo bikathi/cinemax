82bafd496e138c599f8fb36c572b6e5d01c9c58a
Q-T5 <bikathimartin@outlook.com>
2023-07-04 17:04:53 +0300

    Mount movie display component

--- pages/movies.vue
+++ pages/movies.vue
@@ -1,11 +1,20 @@
 <script setup lang="js">
 	definePageMeta({
 	    name: "movies"
-	})
+	});
+	const runtimeConfig = useRuntimeConfig()
 </script>
 
 <template>
-	<main class="flex-grow">
-        <h1>hello world</h1>
-    </main>
+	<main class="flex-grow p-5 grid grid-cols-6 gap-2">
+		<div class="col-span-6">
+			<h1 class="font-roboto text-3xl">Available Movies</h1>
+		</div>
+		<MovieDisplay
+			v-for="a in 10"
+			:key="a"
+			posterLink="/images/extraction-2.jpg"
+			movieId="1234567980347474"
+			movieName="Extraction 2" />
+	</main>
 </template>
