4fe4e507187e5247b91c7deadc6ed42d19c13ef9
Q-T5 <bikathimartin@outlook.com>
2023-07-05 02:10:03 +0300

    Clear the movie store when page re-mounts

--- pages/movies.vue
+++ pages/movies.vue
@@ -1,13 +1,21 @@
 <script setup lang="js">
+	import {useMovieStore} from '~/store/movies.js';
 	definePageMeta({
 	    name: "movies",
 		keepalive: true
-	});
+	})
 	const mountGridLoading = ref(false);
+	const movieStore = useMovieStore();
+	const { clearMovieList } = movieStore;
+
+	onMounted(() => {
+		clearMovieList();
+	})
 </script>
 
 <template>
-	<main class="flex-grow p-5 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-2">
+	<main
+		class="flex-grow p-5 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-2">
 		<Suspense>
 			<MovieGrid
 				@loadMoreMovies="
