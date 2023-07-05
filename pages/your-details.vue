24ff0f94223be23dc468ae6024a87f3dbef64604
Q-T5 <bikathimartin@outlook.com>
2023-07-05 14:04:24 +0300

    Add page to take user details

---
+++ pages/your-details.vue
@@ -0,0 +1,15 @@
+<script setup lang="js">
+	const {data} = useFetch('/api/hello');
+</script>
+
+<template>
+	<main class="flex-grow">
+		<h1>Your details</h1>
+        <h2>{{ data.hello }}</h2>
+		<button
+			class="bg-red-500"
+			@click="tryApi">
+			test api
+		</button>
+	</main>
+</template>
