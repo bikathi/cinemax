b0a40170d6d7414ba6dbcebf099a3d79d6bea430
Q-T5 <bikathimartin@outlook.com>
2023-07-06 23:05:08 +0300

    Make UI more responsive

--- pages/movies.vue
+++ pages/movies.vue
@@ -32,7 +32,7 @@
 				" />
 			<template #fallback>
 				<main
-					class="flex-grow bg-slate-700 opacity-95 backdrop-blur-md w-full flex flex-col justify-center items-center space-y-2">
+					class="flex-grow bg-slate-700 opacity-95 backdrop-blur-md w-full flex flex-col justify-center items-center space-y-2 col-span-2 sm:col-span-3 md:col-span-4 lg:col-span-5 xl:col-span-6 2xl:col-span-7">
 					<breeding-rhombus-spinner
 						:animation-duration="2000"
 						:size="65"
@@ -45,6 +45,6 @@
 		</Suspense>
 		<MovieGridLoading
 			v-if="mountGridLoading === true"
-			v-for="a in 20" />
+			v-for="a in 10" />
 	</main>
 </template>
