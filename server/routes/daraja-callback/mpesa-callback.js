7cadfd1c66b7479c5cdf3bb270f46c164b63268c
Bikathi Martin <bikathimartin@outlook.com>
2023-07-17 19:31:34 +0300

    Add basic logic to payment callback

--- server/routes/daraja-callback/mpesa-callback.js
+++ server/routes/daraja-callback/mpesa-callback.js
@@ -1,5 +1,28 @@
+import { useStkPush } from '../../../stores/stkpush';
+
 export default defineEventHandler(async (event) => {
+	const testState = useTestState();
+
 	console.log('Callback url fired...');
+	console.log(`state of testState: ${testState}`);
 	const transactionResponse = await readBody(event);
 	console.log('Transaction response: ' + JSON.stringify(transactionResponse));
+
+	// stkpush store
+	const stkPushStore = useStkPush();
+	const { setRequestCancelledByUser, setCheckoutRequestIdDarajaCallback } =
+		stkPushStore;
+
+	if (transactionResponse.Body.stkCallback.ResultCode === 1032) {
+		// if result code is 1032, then the request was cancelled by the user
+		// so we update the store accordingly
+		console.log('oops. request cancelled by the user...');
+		setRequestCancelledByUser();
+	}
+
+	// if it's not 1032, then we update the store with the CheckoutRequestId provided in this callback
+	// this signals the UI to update and start validating the transaction
+	setCheckoutRequestIdDarajaCallback(
+		transactionResponse.Body.CheckoutRequestID,
+	);
 });
