d8f10d54f5b408eba08b5273317c0b59ac15a49f
Bikathi Martin <bikathimartin@outlook.com>
2023-07-24 21:32:36 +0300

    Improve UI on fallback component of <Suspence>

--- pages/movies.vue
+++ pages/movies.vue
@@ -4,7 +4,6 @@
 
 	definePageMeta({
 	    name: "movies",
-		keepalive: true
 	})
 	const mountGridLoading = ref(false);
 	const movieStore = useMovieStore();
@@ -32,12 +31,13 @@
 				" />
 			<template #fallback>
 				<main
-					class="flex-grow bg-slate-700 opacity-95 backdrop-blur-md w-full flex flex-col justify-center items-center space-y-2 col-span-2 sm:col-span-3 md:col-span-4 lg:col-span-5 xl:col-span-6 2xl:col-span-7">
+					class="flex-grow w-full flex flex-col justify-center items-center space-y-2 col-span-2 sm:col-span-3 md:col-span-4 lg:col-span-5 xl:col-span-6 2xl:col-span-7">
 					<breeding-rhombus-spinner
 						:animation-duration="2000"
 						:size="65"
-						color="#FFFFFF" />
-					<h1 class="font-ubuntu text-2xl sm:text-3xl text-white">
+						color=" rgb(147 51 234)" />
+					<h1
+						class="font-ubuntu text-2xl sm:text-3xl text-purple-600">
 						Loading...
 					</h1>
 				</main>
