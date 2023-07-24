8263d687e133c51cc1952727ac4f41cb3c4a9b99
Bikathi Martin <bikathimartin@outlook.com>
2023-07-24 21:33:47 +0300

    Ensure we only save if client details do not previously exist

--- server/api/save-client-details.js
+++ server/api/save-client-details.js
@@ -2,26 +2,41 @@ import { prisma } from '../../prisma/db';
 
 export default defineEventHandler(async (event) => {
 	const clientDetails = await readBody(event);
-	const client = await prisma.client.create({
-		data: {
-			firstName: clientDetails.firstName,
-			lastName: clientDetails.lastName,
-			email: clientDetails.email,
+	let client;
+
+	// confirm the client details do not exist yet
+	const existingClient = await prisma.client.findFirst({
+		where: {
 			mobileNumber: `254${clientDetails.mobileNumber}`,
-			location: clientDetails.location,
+			OR: {
+				email: clientDetails.email,
+			},
 		},
 	});
-	if (!client) {
-		return {
-			status: 'FAILED',
-			successfull: false,
-			data: null,
-		};
+
+	// we only save if no clients exist
+	if (!existingClient) {
+		client = await prisma.client.create({
+			data: {
+				firstName: clientDetails.firstName,
+				lastName: clientDetails.lastName,
+				email: clientDetails.email,
+				mobileNumber: `254${clientDetails.mobileNumber}`,
+				location: clientDetails.location,
+			},
+		});
+		if (!client) {
+			return {
+				status: 'FAILED',
+				successfull: false,
+				data: null,
+			};
+		}
 	}
 
 	return {
 		status: 'SUCCESSFULL',
 		successfull: true,
-		data: client,
+		data: client || existingClient,
 	};
 });
